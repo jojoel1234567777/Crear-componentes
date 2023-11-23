805b27f5996ea17afed244be4feaf4ac0393d601
jojoel1234567777 <127361706+jojoel1234567777@users.noreply.github.com>
2023-11-23 15:49:04 -0500

    Add files via upload

---
+++ src/app/pages/avatar/avatar.page.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-avatar',
+  templateUrl: './avatar.page.html',
+  styleUrls: ['./avatar.page.scss'],
+})
+export class AvatarPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onClick(){
+    
+  }
+
+}
